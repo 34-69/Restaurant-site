42fb359791514f75710ccc49f7dca85b881039bf
Souvik <souvik3469@gmail.com>
2022-06-18 01:21:05 +0530

    Frontend done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuCard from './components/basics/MenuCard'
+import Restaurant from './components/basics/Restaurant'
+
+function App() {
+  return (
+    <div>
+      <Restaurant/>
+     
+    </div>
+  )
+}
+
+export default App
