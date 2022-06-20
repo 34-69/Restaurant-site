3c89996ceda404cb166fe99d36b1db38593bcfa7
Souvik <souvik3469@gmail.com>
2022-06-20 20:38:28 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import MenuCard from './components/basics/MenuCard'
 import Restaurant from './components/basics/Restaurant'
+// import UseEffect from './components/Hooks/UseEffect'
+// import UseReducer from './components/Hooks/UseReducer'
+// import UseState from './components/Hooks/UseState'
 
 function App() {
   return (
     <div>
       <Restaurant/>
-     
+      {/* <UseState/> */}
+      {/* <UseEffect/> */}
+      {/* <UseReducer/> */}
     </div>
   )
 }
